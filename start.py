ca8a454bb60a1c0c9a18c32a899bb2b8e26312a0
WT <w.truong@pracht.com>
2022-01-18 16:31:00 +0100

    completed simple measurement programm

--- start.py
+++ start.py
@@ -7,25 +7,48 @@ import pyvisa
 import pandas as pd
 import pytz
 import datetime as dt
+import time
+
+# PyVISA Resource Manager
+rm = pyvisa.ResourceManager()
+# Chroma 66202
+chroma = rm.open_resource('USB0::0x0A69::0x0835::A66200110997::INSTR')
+print(chroma.query("*IDN?"))
+chroma.write("SYSTEM:HEADER ON")
+
+# PeakTech 4095
+peak = rm.open_resource('USB0::0x5345::0x1234::2101323::INSTR')
+print(peak.query("*IDN?"))
+peak.query("CONFigure:VOLTage:DC")
+time.sleep(1.3)
 
 # GUI
 input_column = [
   [sg.Text("Model:")],
   [sg.In("LC 50W", size = (30, 1), enable_events = True, key = "-MODEL-")],
   [sg.Text("Article number:")],
-  [sg.In("1111", size = (30, 1), enable_events = True, key = "-ARTNO-")],
+  [sg.In("-", size = (30, 1), enable_events = True, key = "-ARTNO-")],
   [sg.Text("Output current:")],
   [sg.In("0.30", size = (30, 1), enable_events = True, key = "-CURRENT_OUT-")],
-  [sg.Button("Measure"), sg.Button("Export"), sg.Button("Cancel")]
+  [
+    sg.Button("Measure"), 
+    sg.InputText(visible = False, enable_events = True, key = "-FILE_PATH-"),
+    sg.FileSaveAs( 
+      key = '-FILE_SAVE-', 
+      file_types=(('CSV', '.csv'), ('all', '.*')), 
+      ), 
+    sg.Button("Cancel")
+  ]
 ]
 
-# For Table column
+## For Table column
 data = []
 header_list = [
-  'Model', 'Article number', 'I_out',
-  'datetime', 'P', 'PF', 'THD_v'
+  'Model', 'Article number', 'datetime', 
+  'I_out', 'U_out', 'P_out', 'P', 'efficiency','PF', 'THD_v'
   ]
 
+## window layout
 layout = [
   [
     sg.Column(input_column),
@@ -45,8 +68,6 @@ window = sg.Window("Measurement Software", layout)
 
 while True:
   event, values = window.read()
-  if event == "Cancel" or event == sg.WIN_CLOSED:
-    break
   if event == "Measure":
     ## local PC
     tz = pytz.timezone('Europe/Berlin')
@@ -56,68 +77,33 @@ while True:
     article_number = values['-ARTNO-']
     # try converting to float else error message
     I_out = float(values['-CURRENT_OUT-']) # in A
-    P = 50
-    PF = 0.95
-    THD_v = 0.07
-    
-    ## Multimeter
-    U_out = 130
-    P_out = I_out * U_out
 
-    efficiency = P_out / P
-    data.append([model, article_number, I_out, datetime, P, PF, THD_v])
+    ## Chroma 66202
+    chroma.query("MEASure?")
+    P = float(chroma.query("FETCh:POWer:REAL?").split()[1])
+    PF = float(chroma.query("FETCh:POWer:PFACtor?").split()[1])
+    THD_v = float(chroma.query("FETCh:VOLTage:THD?").split()[1])
+    
+    ## PeakTech 4095
+    U_out = float(peak.query("MEAS1?"))
+    P_out = I_out * U_out
+    if P > 0:
+      efficiency = P_out / P
+    else:
+      efficiency = 0
+    data.append([
+      model, article_number, datetime, 
+      I_out, U_out, P_out, P, efficiency, PF, THD_v
+      ])
     window["-table-"].update(
       values = data, 
       num_rows = min(10, len(data))
       )
-  if event == "Export":
+  if (event == '-FILE_PATH-') and (values['-FILE_PATH-'] != ''):
+    print('Saving to:', values['-FILE_PATH-'])
+    df = pd.DataFrame(data, columns = header_list)
+    df.to_csv(values['-FILE_PATH-'], index = False)
+  if event == "Cancel" or event == sg.WIN_CLOSED:
     break
 
 window.close()
-
-
-rm = pyvisa.ResourceManager()
-print(rm.list_resources())
-
-inst = rm.open_resource('USB0::0x0A69::0x0835::A66200110997::INSTR')
-print(inst.query("*IDN?"))
-
-inst.write("SYSTEM:HEADER ON")
-
-print(inst.query("MEASure?"))
-
-# User inputs
-model = "LC 50"
-article_number = "28000680"
-I_out = "0.300" # in A
-
-# measurement data
-## local PC
-tz = pytz.timezone('Europe/Berlin')
-datetime = dt.datetime.now(tz)
-
-## Chroma 66202
-P = inst.query("FETCh:POWer:REAL?").split()[1]
-PF = inst.query("FETCh:POWer:PFACtor?").split()[1]
-THD_v = inst.query("FETCh:VOLTage:THD?").split()[1]
-
-P = 50
-PF = 0.95
-THD_v = 0.07
-
-## Multimeter
-U_out = 130
-P_out = I_out * U_out
-
-efficiency = P_out / P
-
-
-data = []
-data.append([model, article_number, I_out, datetime, P, PF, THD_v])
-
-df = pd.DataFrame(data, columns = [
-    'model', 'article_number', 'I_out',
-    'datetime', 'P', 'PF', 'THD_v'
-    ])
-
-str(df)
